356fddbe82431cfd89fd3e6a091526163ef9b750
Suman <72369336+SumanSunuwar@users.noreply.github.com>
2022-10-02 17:53:02 +0545

    features: adding ratings and equiries

---
+++ apps/enquiries/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from phonenumber_field.modelfields import PhoneNumberField
+from apps.common.models import TimeStampedUUIDModel
+
+
+class Enquiry(TimeStampedUUIDModel):
+    name = models.CharField(
+        verbose_name = _("Your Name"),
+        max_length = 100
+    )
+    email = models.EmailField(
+        verbose_name = _("Email")
+    )
+    phone_number = PhoneNumberField(
+        verbose_name = _("Phone Number"),
+        max_length = 30,
+        default = "+123456789012"
+    )
+    subject = models.TextField(
+        verbose_name = _("Subject"),
+        max_length = 100
+    )
+    message = models.TextField(
+        verbose_name = _("Message")
+    )
+
+    def __str__(self) -> str:
+        return self.email
+
+    class Meta:
+        verbose_name_plural = "Enquiries"
---
+++ apps/enquiries/views.py
@@ -0,0 +1,46 @@
+import imp
+from django.core.mail import send_mail
+
+from real_estate.settings.development import DEFAULT_FROM_EMAIL
+from rest_framework import permissions
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+
+from .models import Enquiry
+
+
+@api_view(["POST"])
+@permission_classes([permissions.AllowAny])
+def send_enquiry_email(request):
+    data = request.data
+    try:
+        subject = data["subject"]
+        name = data["name"]
+        email = data["email"]
+        message = data["message"]
+        from_email = data["email"]
+        recipent_list = [DEFAULT_FROM_EMAIL]
+
+        send_mail(
+            subject,
+            message,
+            from_email,
+            recipent_list,
+            fail_silently=True
+        )
+
+        enquiry = Enquiry(
+            name = name,
+            email = email,
+            subject = subject,
+            message = message
+        )
+        enquiry.save()
+
+        return Response({
+            "success" : "Successfully sent the enquiry"
+        })
+    except:
+        return Response({
+            "fail" : "Failed to send the enquiry...Try again!!!"
+        })
